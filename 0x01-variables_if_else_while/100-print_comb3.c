8250cd6567d57410ed86c911d4cadc1f7b18fd8d
IsmailMahraan <112779065+IsmailMahraan@users.noreply.github.com>
2023-08-13 00:48:26 +0300

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -18,29 +18,21 @@
  */
 int main(void)
 {
-	int c;
-	int d = 0;
+	int x = 0;
+	int y = 0;
 
-	while (d < 10)
+	while (y < 10)
 	{
-		c = 0;
-		while (c < 10)
+		while (x < 10)
 		{
-			if (d != c && d < c)
+			if (y != x && y < x)
 			{
-				putchar('0' + d);
-				putchar('0' + c);
-
-				if (c + d != 17)
-				{
-					putchar(',');
-					putchar(' ');
-				}
+				putchar('0' + y);
+				putchar('0' + x);
 			}
-
-			c++;
+			y++;
 		}
-		d++;
+		c++;
 	}
 	putchar('\n');
 	return (0);
